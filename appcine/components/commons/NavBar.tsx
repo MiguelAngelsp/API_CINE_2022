d1b374e0947ba903ef24b95cf8259fc4e239e25f
MiguelAngelsp <miguel.correopers@gmail.com>
2023-03-09 16:52:29 +0100

    Rediseño_NavBar_APP_09/03/2023

--- appcine/components/commons/NavBar.tsx
+++ appcine/components/commons/NavBar.tsx
@@ -6,7 +6,7 @@ import { useContext } from "react";
 import { AuthContext } from "@/context/auth/AuthContext";
 
 export const NavBar = () => {
-    const { user } =  useContext(AuthContext); 
+    const { user } = useContext(AuthContext);
     return (
         <AppBar sx={{ backgroundColor: 'Brown' }}>
             <Toolbar>
@@ -14,7 +14,7 @@ export const NavBar = () => {
 
                 <Link href='/' passHref component={NextLink}>
                     <Button sx={{ color: 'white' }}>Home</Button>
-                    { user?.fullName }/{ user?.Correo }/{ user?.roles }
+                    {user?.fullName}/{user?.Correo}/{user?.roles}
                 </Link>
                 <Box flex={1} />
 
@@ -22,27 +22,80 @@ export const NavBar = () => {
                     sx={{ display: { xs: 'none', sm: 'flex' } }} >
 
                     <Link href='/auth' passHref component={NextLink}>
-                        <Button sx={{ color: 'white' }}>Auth</Button>
+
+                        <Button sx={{ color: 'white' }}>
+                            <Image
+                                src='/auth.png'
+                                alt='Descripción de la imagen'
+                                width={30}
+                                height={30}
+                            />
+                            Auth</Button>
+
                     </Link>
                     <Link href='/cesta' component={NextLink}>
-                        <Button sx={{ color: 'white' }}>Cesta</Button>
+
+                        <Button sx={{ color: 'white' }}>
+                        <Image
+                                src='/cesta.png'
+                                alt='Descripción de la imagen'
+                                width={30}
+                                height={30}
+                            />
+                            Cesta</Button>
+
                     </Link>
                     <Link href='/generos' component={NextLink}>
-                        <Button sx={{ color: 'white' }}>Géneros</Button>
+                        <Button sx={{ color: 'white' }}>
+                        <Image
+                                src='/generos.webp'
+                                alt='Descripción de la imagen'
+                                width={30}
+                                height={30}
+                            />
+                            Géneros</Button>
                     </Link>
                     <Link href='/peliculas' component={NextLink}>
-                        <Button sx={{ color: 'white' }}>Películas</Button>
+                        <Button sx={{ color: 'white' }}>
+                        <Image
+                                src='/peliculas.png'
+                                alt='Descripción de la imagen'
+                                width={30}
+                                height={30}
+                            />
+                            Películas</Button>
                     </Link>
                     <Link href='/usuarios' component={NextLink}>
-                        <Button sx={{ color: 'white' }}>Usuarios</Button>
+                        <Button sx={{ color: 'white' }}>
+                        <Image
+                                src='/usuarios.png'
+                                alt='Descripción de la imagen'
+                                width={30}
+                                height={30}
+                            />
+                            Usuarios</Button>
                     </Link>
                     <Link href='/valoraciones' component={NextLink}>
-                        <Button sx={{ color: 'white' }}>Valoraciones</Button>
+                        <Button sx={{ color: 'white' }}>
+                        <Image
+                                src='/valoraciones.png'
+                                alt='Descripción de la imagen'
+                                width={30}
+                                height={30}
+                            />
+                            Valoraciones</Button>
                     </Link>
                 </Box>
                 <Box flex={1} />
                 <Link href='/auth/login' passHref component={NextLink}>
-                    <Button sx={{ color: 'white' }}>Login</Button>
+                    <Button sx={{ color: 'white' }}>
+                    <Image
+                                src='/logeo.png'
+                                alt='Descripción de la imagen'
+                                width={30}
+                                height={30}
+                            />
+                        Login</Button>
                 </Link>
                 <Box flex={1} />
                 <IconButton
