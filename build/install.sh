fb1d019a66396bb1e2d8692b8c37c55d823c1c89
MiguelAngelsp <miguel.correopers@gmail.com>
2023-03-13 20:53:58 +0100

    Cambios15_Deploy_Jenkins_13-03-2023

--- build/install.sh
+++ build/install.sh
@@ -1,5 +1,5 @@
 echo "Desplegando la app de gstión de un Cine"
-export DOCKER_HOST="tcp://192.168.16.163:3000"
+export DOCKER_HOST="tcp://192.168.16.163"
 docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/build/docker-compose.yml build
 docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/build/docker-compose.yml down
 docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/build/docker-compose.yml up -d
