cc711a000cc2355163aeff1cc487ab1041f7b0d9
MiguelAngelsp <miguel.correopers@gmail.com>
2023-03-13 14:35:53 +0100

    Cambios4_Deploy_Jenkins_13-03-2023

--- build/install.sh
+++ build/install.sh
@@ -1,4 +1,4 @@
 echo "Desplegando la app de gstión de un Cine"
-docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api_nest/docker-compose.yml build
-docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api_nest/docker-compose.yml down
-docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api_nest/docker-compose.yml up -d
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml build
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml down
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml up -d
